27818b9eb0cae1046d348ea161073758b2e604b9
Ben Schreiber <bpschreiber2003@gmail.com>
2025-09-07 22:53:31 -0700

    fix snaps

--- src/generator/client/tests/client_tests.rs
+++ src/generator/client/tests/client_tests.rs
@@ -14,7 +14,7 @@ fn test_generate_client_snapshot() -> Result<()> {
     };
 
     // Act
-    let client = client::generate_client_api(cidl, "localhost:1000".to_string());
+    let client = client::generate_client_api(cidl, "http://localhost:1000/api".to_string());
 
     // Assert
     assert_snapshot!("generated_client", client);
