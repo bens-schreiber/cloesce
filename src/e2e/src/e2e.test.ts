c3991bac738490f6c7a57df49e16d8128b65044a
Ben Schreiber <bpschreiber2003@gmail.com>
2025-09-23 21:01:02 -0700

    add GET related stuff back in

--- src/e2e/src/e2e.test.ts
+++ src/e2e/src/e2e.test.ts
@@ -17,7 +17,7 @@ before(
     Horse = mod.Horse;
     Match = mod.Match;
   },
-  { timeout: 30_000 },
+  { timeout: 30_000 }
 );
 
 test("Post, Patch, Get a Horse", async () => {
@@ -31,7 +31,7 @@ test("Post, Patch, Get a Horse", async () => {
   assert.ok(res.ok, "POST should be OK");
   assert.ok(
     res.data.id == body.id,
-    "POST response id should be the same as the inputted id",
+    "POST response id should be the same as the inputted id"
   );
 
   body.name = "ROACH";
@@ -39,83 +39,80 @@ test("Post, Patch, Get a Horse", async () => {
   res = await horse.patch(body);
   assert.ok(res.ok, "PATCH should be OK");
 
-  // TODO: Test GET once that works for client
-  // res = await Horse.get(body.id);
-  // console.log(res);
-  // assert.ok(res.ok, "GET should be OK");
-  // assert.ok(
-  //   res.data.id == body.id,
-  //   "GET response id should be the same as the inputted id"
-  // );
+  res = await Horse.get(body.id);
+  console.log(res);
+  assert.ok(res.ok, "GET should be OK");
+  assert.ok(
+    res.data.id == body.id,
+    "GET response id should be the same as the inputted id"
+  );
 });
 
-// TODO: Uncomment when GET works for the client
-// test("List horse returns all horses", async () => {
-//   let res = await Horse.list();
-//   assert.ok(res.ok);
-//   let horses = res.data;
-//   assert.equal(horses.length, 1);
+test("List horse returns all horses", async () => {
+  let res = await Horse.list();
+  assert.ok(res.ok);
+  let horses = res.data;
+  assert.equal(horses.length, 1);
 
-//   let newHorses = [
-//     {
-//       id: 2,
-//       name: "sonic",
-//       bio: "the horse",
-//       matches: [],
-//     },
-//     {
-//       id: 3,
-//       name: "other roach",
-//       bio: "geralts other horse",
-//       matches: [],
-//     },
-//   ];
+  let newHorses = [
+    {
+      id: 2,
+      name: "sonic",
+      bio: "the horse",
+      matches: [],
+    },
+    {
+      id: 3,
+      name: "other roach",
+      bio: "geralts other horse",
+      matches: [],
+    },
+  ];
 
-//   let postResults = await Promise.all(newHorses.map((h) => Horse.post(h)));
-//   postResults.forEach((res) => assert.ok(res.ok));
+  let postResults = await Promise.all(newHorses.map((h) => Horse.post(h)));
+  postResults.forEach((res) => assert.ok(res.ok));
 
-//   res = await Horse.list();
-//   assert.ok(res.ok);
-//   assert.equal(res.data.length, 3);
+  res = await Horse.list();
+  assert.ok(res.ok);
+  assert.equal(res.data.length, 3);
 
-//   // Node's assert doesn't have `arrayContaining`, so use deepEqual after sorting
-//   const allHorses = [...horses, ...newHorses];
-//   assert.deepEqual(
-//     res.data.sort((a: any, b: any) => a.id - b.id),
-//     allHorses.sort((a: any, b: any) => a.id - b.id)
-//   );
-// });
+  // Node's assert doesn't have `arrayContaining`, so use deepEqual after sorting
+  const allHorses = [...horses, ...newHorses];
+  assert.deepEqual(
+    res.data.sort((a: any, b: any) => a.id - b.id),
+    allHorses.sort((a: any, b: any) => a.id - b.id)
+  );
+});
 
-// TODO: Uncomment once GET works for the client
-// test("Horse can match with another horse", async () => {
-//   let res = await Horse.get(0);
-//   assert.ok(res.ok);
-//   let horse1 = res.data;
+test("Horse can match with another horse", async () => {
+  let res = await Horse.get(0);
+  assert.ok(res.ok);
+  let horse1 = res.data;
 
-//   res = await Horse.get(1);
-//   assert.ok(res.ok);
-//   let horse2 = res.data;
+  res = await Horse.get(1);
+  assert.ok(res.ok);
+  let horse2 = res.data;
 
-//   res = await horse1.match(horse2);
-//   assert.ok(res.ok);
+  res = await horse1.match(horse2);
+  assert.ok(res.ok);
 
-//   res = await Horse.get(horse1.id);
-//   assert.ok(res.ok);
-//   let updated_horse1 = res.data;
+  res = await Horse.get(horse1.id);
+  assert.ok(res.ok);
+  let updated_horse1 = res.data;
 
-//   res = await Horse.get(horse2.id);
-//   assert.ok(res.ok);
-//   let updated_horse2 = res.data;
+  res = await Horse.get(horse2.id);
+  assert.ok(res.ok);
+  let updated_horse2 = res.data;
 
-//   assert.equal(horse1.matches.length, 1);
-//   assert.equal(horse2.matches.length, 1);
-//   assert.ok(updated_horse1.matches.includes(horse2.id));
-//   assert.ok(updated_horse2.matches.includes(horse1.id));
-// });
+  assert.equal(horse1.matches.length, 1);
+  assert.equal(horse2.matches.length, 1);
+  assert.ok(updated_horse1.matches.includes(horse2.id));
+  assert.ok(updated_horse2.matches.includes(horse1.id));
+});
 
-// test("Default include tree shows all matches but goes no further", async () => {
-//   let res = await Horse.get(0);
-//   assert.ok(res.ok);
-//   let horse1 = res.data;
-//   assert.equal(horse1.matches[0].matches.length, 0);
-// });
+test("Default include tree shows all matches but goes no further", async () => {
+  let res = await Horse.get(0);
+  assert.ok(res.ok);
+  let horse1 = res.data;
+  assert.equal(horse1.matches[0].matches.length, 0);
+});
